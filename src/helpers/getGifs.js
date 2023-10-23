07c3f3571aa89d16d15f46acfecbfc1bc4688bcb
Alejandro Ureña Ballesteros <alejandroub@ext.zara.com>
2023-10-23 16:50:35 +0200

    fix Netlify deploy error (use HTTPS, not HTTP)

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
     const apiKey = '8QieHB8JR0msLbSwwov9pwz8rJL0MeVl';
-    const host = 'http://api.giphy.com';
+    const host = 'https://api.giphy.com';
     const limit = 10;
     const url = `${host}/v1/gifs/search?q=${category}&api_key=${apiKey}&limit=${limit}`;
 
